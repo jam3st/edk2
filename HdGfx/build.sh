2e36df4859d533d5b7e94331b3392d91cf74ff1f
Your Name <you@example.com>
2018-07-23 01:51:36 +0000

    ramstage

--- HdGfx/build.sh
+++ HdGfx/build.sh
@@ -3,6 +3,7 @@
 bd=${PWD}/build
 cc=/usr/x86_64-pc-linux-gnu/gcc-bin/7.3.1/x86_64-pc-linux-gnu-gcc
 ar=/usr/x86_64-pc-linux-gnu/gcc-bin/7.3.1/x86_64-pc-linux-gnu-gcc-ar
+gb=/usr/x86_64-pc-linux-gnu/gcc-bin/7.3.1/x86_64-pc-linux-gnu-gnatbind
 rm -rf ${bd}
 mkdir -p build/adalib build/adainclude build/lib/gnat
 
@@ -23,7 +24,7 @@ ${ar} cr ${bd}/libgnat.a ${bd}/lib/gnat/ada.o ${bd}/lib/gnat/a-unccon.o ${bd}/li
 ##### GNAT #####
 ##### HWBASE #####
 mkdir ${bd}/libhwbase
-for i in glue/hw-debug_sink.adb libhwbase/ada/dynamic_mmio/hw-mmio_range.adb libhwbase/debug/hw-debug.adb \
+for i in glue/hw-debug_sink.adb glue/hw-time-timer.adb libhwbase/ada/dynamic_mmio/hw-mmio_range.adb libhwbase/debug/hw-debug.adb \
     libhwbase/common/direct/hw-pci-dev.adb libhwbase/common/hw-mmio_regs.adb libhwbase/common/hw-pci-mmconf.adb \
     libhwbase/common/hw-port_io.adb libhwbase/common/hw-time.adb ; do
     mkdir -p ${bd}/`dirname ${i}`
@@ -40,7 +41,8 @@ done
 ##### HWBASE #####
 ##### LIBGFXINIT #####
 mkdir ${bd}/libgfxinit
-for i in glue/gma.adb libgfxinit/common/haswell_shared/hw-gfx-gma-connectors-ddi.adb libgfxinit/common/haswell_shared/hw-gfx-gma-connectors-ddi.adb libgfxinit/common/haswell_shared/hw-gfx-gma-port_detect.adb \
+for i in glue/gma.adb libgfxinit/common/haswell_shared/hw-gfx-gma-connectors-ddi.adb libgfxinit/common/haswell_shared/hw-gfx-gma-connectors-ddi.adb \
+    libgfxinit/common/haswell_shared/hw-gfx-gma-connectors.adb libgfxinit/common/haswell_shared/hw-gfx-gma-port_detect.adb \
     libgfxinit/common/haswell_shared/hw-gfx-gma-power_and_clocks_haswell.adb libgfxinit/common/haswell/hw-gfx-gma-connectors-ddi-buffers.adb \
     libgfxinit/common/haswell/hw-gfx-gma-plls-wrpll.adb libgfxinit/common/haswell/hw-gfx-gma-plls.adb libgfxinit/common/haswell/hw-gfx-gma-spll.adb \
     libgfxinit/common/hw-gfx-dp_aux_ch.adb libgfxinit/common/hw-gfx-dp_info.adb libgfxinit/common/hw-gfx-dp_training.adb libgfxinit/common/hw-gfx-edid.adb libgfxinit/common/hw-gfx-framebuffer_filler.adb \
@@ -54,11 +56,26 @@ for i in glue/gma.adb libgfxinit/common/haswell_shared/hw-gfx-gma-connectors-ddi
     -c ${i} -o ${bd}/`dirname ${i}`/`basename -s .adb ${i}`.o
 done
 
-for i in libgfxinit/common/haswell/hw-gfx-gma-ddi_phy.ads libgfxinit/common/haswell_shared/hw-gfx-gma-ddi_phy_stub.ads libgfxinit/common/haswell/hw-gfx-gma-ddi_phy.ads \
+for i in glue/gma-mainboard.ads libgfxinit/common/haswell/hw-gfx-gma-ddi_phy.ads libgfxinit/common/haswell_shared/hw-gfx-gma-ddi_phy_stub.ads libgfxinit/common/haswell/hw-gfx-gma-ddi_phy.ads \
     libgfxinit/common/haswell/hw-gfx-gma-plls-lcpll.ads libgfxinit/common/haswell/hw-gfx-gma-power_and_clocks.ads libgfxinit/common/hw-gfx-dp_defs.ads libgfxinit/common/hw-gfx-gma-dp_aux_ch.ads \
-    libgfxinit/common/hw-gfx-gma-dp_info.ads libgfxinit/common/hw-gfx-i2c.ads libgfxinit/common/hw-gfx.ads libgfxinit/common/hw-gfx-gma-config.ads ; do
+    libgfxinit/common/hw-gfx-gma-dp_info.ads libgfxinit/common/hw-gfx-i2c.ads libgfxinit/common/hw-gfx.ads libgfxinit/common/hw-gfx-gma-config.ads libgfxinit/common/hw-gfx-gma-pch.ads ; do
     mkdir -p ${bd}/`dirname ${i}`
     ${cc} --RTS=${bd} -gnatp -m64 -fuse-ld=bfd -fno-stack-protector -Wl,--build-id=none -ffunction-sections -fdata-sections -gnatwa.eeD.HHTU.U.W.Y -gnatyN -Os -gnata -march=x86-64 \
     -Ilibhwbase/common/direct -Ilibhwbase/ada/dynamic_mmio -Ilibhwbase/common -Ilibhwbase/debug -Ilibgfxinit/common/haswell_shared -Ilibgfxinit/common/haswell -Ilibgfxinit/common \
     -c ${i} -o ${bd}/`dirname ${i}`/`basename -s .ads ${i}`.o
 done
+##### LIBGFXINIT #####
+cd ${bd}
+${gb} -a -n --RTS=${bd} -Lhdgfx_ada -o hdgdx.adb \
+    libgfxinit/common/haswell/hw-gfx-gma-connectors-ddi-buffers.ali libgfxinit/common/haswell/hw-gfx-gma-ddi_phy.ali libgfxinit/common/haswell/hw-gfx-gma-plls-lcpll.ali \
+    libgfxinit/common/haswell/hw-gfx-gma-plls-wrpll.ali libgfxinit/common/haswell/hw-gfx-gma-plls.ali libgfxinit/common/haswell/hw-gfx-gma-power_and_clocks.ali libgfxinit/common/haswell/hw-gfx-gma-spll.ali \
+    libgfxinit/common/haswell_shared/hw-gfx-gma-connectors-ddi.ali libgfxinit/common/haswell_shared/hw-gfx-gma-connectors.ali libgfxinit/common/haswell_shared/hw-gfx-gma-ddi_phy_stub.ali \
+    libgfxinit/common/haswell_shared/hw-gfx-gma-port_detect.ali libgfxinit/common/haswell_shared/hw-gfx-gma-power_and_clocks_haswell.ali libgfxinit/common/hw-gfx-dp_aux_ch.ali \
+    libgfxinit/common/hw-gfx-dp_defs.ali libgfxinit/common/hw-gfx-dp_info.ali libgfxinit/common/hw-gfx-dp_training.ali libgfxinit/common/hw-gfx-edid.ali libgfxinit/common/hw-gfx-framebuffer_filler.ali \
+    libgfxinit/common/hw-gfx-gma-config_helpers.ali libgfxinit/common/hw-gfx-gma-connector_info.ali libgfxinit/common/hw-gfx-gma-display_probing.ali libgfxinit/common/hw-gfx-gma-dp_aux_ch.ali \
+    libgfxinit/common/hw-gfx-gma-dp_aux_request.ali libgfxinit/common/hw-gfx-gma-dp_info.ali libgfxinit/common/hw-gfx-gma-i2c.ali libgfxinit/common/hw-gfx-gma-panel.ali \
+    libgfxinit/common/hw-gfx-gma-pch-fdi.ali libgfxinit/common/hw-gfx-gma-pch-sideband.ali libgfxinit/common/hw-gfx-gma-pch-transcoder.ali libgfxinit/common/hw-gfx-gma-pch-vga.ali \
+    libgfxinit/common/hw-gfx-gma-pch.ali libgfxinit/common/hw-gfx-gma-pipe_setup.ali libgfxinit/common/hw-gfx-gma-registers.ali libgfxinit/common/hw-gfx-gma-transcoder.ali libgfxinit/common/hw-gfx-gma.ali \
+    libgfxinit/common/hw-gfx-i2c.ali libgfxinit/common/hw-gfx.ali libhwbase/ada/dynamic_mmio/hw-mmio_range.ali libhwbase/common/direct/hw-pci-dev.ali libhwbase/common/hw-mmio_regs.ali \
+    libhwbase/common/hw-pci-mmconf.ali libhwbase/common/hw-pci.ali libhwbase/common/hw-port_io.ali libhwbase/common/hw-sub_regs.ali libhwbase/common/hw-time.ali libhwbase/common/hw.ali \
+    libhwbase/debug/hw-debug.ali libgfxinit/common/hw-gfx-gma-config.ali libhwbase/common/hw-config.ali glue/hw-debug_sink.ali glue/gma.ali glue/hw-time-timer.ali glue/gma-mainboard.ali
